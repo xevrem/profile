c03e03b12ad1db48043479645f601c00ce9a3f20
erika jonell <erika.jonell@gmail.com>
2018-04-07 09:17:25 -0400

    moved contact info into nav added work history section variablized alot of the colors began tweaks for color space added additional project

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 import project_data from './../projects.json';
+import history_data from './../history.json';
 
 const Navbar = (props) => {
   return (
     <div className="navbar">
       <ul className="nav">
-        <li id="name"><p>Erika Jonell</p></li>
-        <li><a href="#about-container" target="_self">About</a></li>
-        <li><a href="#project-container" target="_self">Projects</a></li>
-        <li><a href="#contact-container" target="_self">Contact</a></li>
+        <li className="left" id="name"><p>Erika Jonell</p></li>
+        {/* <li className="left"><a href="#about-container" target="_self">About</a></li> */}
+        <li className="left"><a href="#project-container" target="_self">Projects</a></li>
+        <li className="left"><a href="#history-container" target="_self">History</a></li>
+        <li className="right"><a href="#email"><i className="fa fa-envelope-o" aria-hidden="true"></i></a></li>
+        <li className="right"><a href="https://github.com/xevrem"><i className="fa fa-github" aria-hidden="true"></i></a></li>
+        <li className="right"><a href="https://www.linkedin.com/in/erika-jonell"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
       </ul>
     </div>
   );
 }
 
+
 const SKILLS = {
   'Software Languages': 'C#, Java, JavaScript, Python, and SQL',
-  'Frameworks & Standards': 'Standards (e.g., CSS, HTML, JSON, ServiceWorkers, and XML/XSD), JS/Node (e.g., Axios, Express, NPM/Yarn, React, Vue), Python (e.g., Flask, Jinja2, Jupyter, Keras, Matplotlib, Numpy, Scikit-Learn, Tensorflow), and Styling (e.g., Bootstrap, Font Awesome)',
+  'Frameworks & Standards': 'Standards (e.g., CSS, HTML, JSON, ServiceWorkers, and XML/XSD), JS/Node (e.g., Axios, Express, NPM/Yarn, React, Vue), Python (e.g., Flask, Jupyter, Keras, Matplotlib, Numpy, Scikit-Learn, Tensorflow), and Styling (e.g., Bootstrap, Font Awesome)',
   'Databases': 'MongoDB, IndexedDB, and SQLite',
   'Development Tools': 'Atom, MonoDevelop, and Visual Studio',
   'Lifecycle Management':'IBM Rational, Git, and GitHub',
@@ -40,9 +45,9 @@ const About = (props) =>{
   return (
     <div id="about-container">
       <div className="header">
-        <h2>Erika Jonell</h2>
+        <h2>About Erika</h2>
+        <h3 className="self-title">Software Engineer</h3>
       </div>
-      <h3 className="self-title">Software Engineer</h3>
       <hr className="my-hr"/>
       <div className="about-grid">
         <div className="about-img">
@@ -84,6 +89,7 @@ const Projects = (props) => {
       <div className="header">
         <h2>Past Projects</h2>
       </div>
+      <hr className="my-hr"/>
       <div className="grid-container">
         {project_list}
       </div>
@@ -91,19 +97,48 @@ const Projects = (props) => {
   );
 }
 
-const ContactInfo = (props) => {
+// const ContactInfo = (props) => {
+//   return (
+//     <div id="contact-container">
+//       <div className="header">
+//         <h2>Contact Methods</h2>
+//       </div>
+//       <hr className="my-hr"/>
+//       <div className="contact-block">
+//         <a href="#email"><i className="fa fa-envelope-o" aria-hidden="true"></i></a>
+//         <a href="https://github.com/xevrem"><i className="fa fa-github" aria-hidden="true"></i></a>
+//         <a href="#twitter"><i className="fa fa-twitter" aria-hidden="true"></i></a>
+//       </div>
+//     </div>
+//   );
+// }
+//
+
+const History = (props) => {
+
+  let history_list = props.history.map((item, i)=>{
+    return (
+      <li className='history-item'>
+        <h3><u>{item.title}</u></h3>
+        <h4><em>{item.entity} ( {item.dates} )</em></h4>
+        <p>{item.description}</p>
+      </li>
+    )
+  })
+
   return (
-    <div id="contact-container">
+    <div id='history-container'>
       <div className="header">
-        <h2>Contact Methods</h2>
+        <h2>Work History</h2>
       </div>
-      <div className="contact-block">
-        <a href="#email"><i className="fa fa-envelope-o" aria-hidden="true"></i></a>
-        <a href="https://github.com/xevrem"><i className="fa fa-github" aria-hidden="true"></i></a>
-        <a href="#twitter"><i className="fa fa-twitter" aria-hidden="true"></i></a>
+      <hr className="my-hr"/>
+      <div className="history-block">
+        <ul className='history-list'>
+          {history_list}
+        </ul>
       </div>
     </div>
-  );
+  )
 }
 
 const Section = props => {
@@ -119,7 +154,8 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      projects: project_data
+      projects: project_data,
+      history: history_data
     }
   }
   render() {
@@ -137,7 +173,7 @@ class App extends Component {
           </Section>
 
           <Section>
-            <ContactInfo />
+            <History history={this.state.history}/>
           </Section>
 
         </div>
