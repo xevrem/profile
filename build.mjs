6cd47a35da83edb44386c0383b999251d055fd28
erika jonell <erika.jonell@gmail.com>
2024-01-01 11:03:25 -0500

    initial ecsf addition

---
+++ build.mjs
@@ -0,0 +1,12 @@
+import * as esbuild from "esbuild";
+import { inlineSass } from "esbuild-inline-sass";
+
+await esbuild.build({
+  bundle: true,
+  entryPoints: ["./src/index.jsx"],
+  minify: true,
+  outdir: "./public",
+  plugins: [inlineSass()],
+  sourcemap: false,
+  target: ["es2015"],
+});
